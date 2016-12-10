17e97805720eca27e7e561e9abedb57ab737bdd8
Che B <cnakamura336@gmail.com>
2016-12-10 18:05:35 -0500

    update img

--- ship.py
+++ ship.py
@@ -6,7 +6,7 @@ class Ship():
         self.screen = screen
         # load the ship img and get its rect
         # self.image = pygame.image.load("img-file/space-ship-148536_640.png")
-        self.image = pygame.image.load("img-file/space-shattle.png")
+        self.image = pygame.image.load("img-file/small-ship.png")
         # self.image_size = self.image.
         # self.image_height = 70
         # self.image_width = 50
