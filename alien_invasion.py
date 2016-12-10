e0a94e7e8388d0717e25a0af3752dedc97d4dbf1
Che B <cnakamura336@gmail.com>
2016-12-10 17:17:06 -0500

    update n refactore

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,10 @@
-import sys
-
 import pygame
 
 # Gets the class values from setting.py
 from settings import Settings
+from ship import Ship
+import game_functions as gf
+
 
 def run_game():
     # Initialize game and creat a screen object
@@ -12,17 +13,17 @@ def run_game():
     screen = pygame.display.set_mode ((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invation")
     # Set the back ground color
-
+    # Make a ship
+    ship = Ship(screen)
     # Start the main loop for the game
     while True:
+        gf.check_events()
         # Mach for keybord and mouse events
         screen.fill(ai_settings.bg_color)
         # While it'S True it updates the page color
+        ship.blitme()
         pygame.display.flip()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
     # Make the most recently screen visible.
 
 run_game()
--- ship.py
+++ ship.py
@@ -1,17 +1,20 @@
 import pygame
 
-class ship():
-
+class Ship():
     def __init__(self, screen):
         """ Initialize the ship and set it starting potition"""
         self.screen = screen
         # load the ship img and get its rect
-        slef.image = pygame.image.load("img-file/space-ship-148536_640.png")
+        # self.image = pygame.image.load("img-file/space-ship-148536_640.png")
+        self.image = pygame.image.load("img-file/space-shattle.png")
+        # self.image_size = self.image.
+        # self.image_height = 70
+        # self.image_width = 50
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-        # start each new ship from the button from the screen
+        # start each new ship from the buttom from the screen
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.botton = self.screen_rect.button
+        self.rect.bottom = self.screen_rect.bottom
 
 
     def blitme(self):
