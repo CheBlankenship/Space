aa9173c0383a3088f4d5e593b0511ff7d42564b6
Che B <cnakamura336@gmail.com>
2016-12-11 21:13:17 -0500

    firing bullets middle

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,3 +22,32 @@ def run_game():
         ship.blitme()
 
 run_game()
+
+
+# import sys
+# import pygame
+#
+# from pygame.sprite import Group
+# from settings import Settings
+# from ship import Ship
+# import game_functions as gf
+#
+#
+# def run_game():
+#     pygame.init()
+#     ai_settings = Settings()
+#     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+#     pygame.display.set_caption("Alien")
+#     bg_color = (0, 0, 102)
+#     screen.fill(bg_color)
+#     ship = Ship(ai_settings, screen)
+#     bullets = Group()
+#
+#     while True:
+#         gf.check_events(ai_settings, screen, ship, bullets)
+#         ship.update()
+#         bullets.update()
+#         gf.update_screen(ai_settings, screen, ship, bullets)
+#         ship.blitme()
+#
+# run_game()
--- settings.py
+++ settings.py
@@ -4,3 +4,17 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (0, 0, 102)
         # self.ship_speed_factor = 1.5
+
+
+# class Settings():
+#     def __init__(self):
+#         self.screen_width = 1200
+#         self.screen_height = 800
+#         self.bg_color = (0, 0, 102)
+#         # self.ship_speed_factor = 1.5
+#
+#         # Bullet Settings
+#         self.bullet_speed_factor = 5
+#         self.bullet_width = 3
+#         self.bullet_height = 15
+#         self.bullet_color = 153, 255, 51
